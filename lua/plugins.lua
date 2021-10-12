aa82c956e79c3b2783a31086c49a1fa16b031045
josh <41476440+Joshua-Hwang@users.noreply.github.com>
2021-10-12 10:54:50 +1000

    added plugins folder

---
+++ lua/plugins.lua
@@ -0,0 +1,31 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-endwise'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-unimpaired'
+  use 'tpope/vim-speeddating'
+  use 'rstacruz/vim-closer'
+  use 'andymass/vim-matchup'
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}, {'ryanoasis/vim-devicons'}}
+  }
+
+  use 'neovim/nvim-lspconfig'
+
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  use 'hrsh7th/nvim-compe'
+
+  use 'sainnhe/sonokai'
+end)
